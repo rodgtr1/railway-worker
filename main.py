f24733d7a7ff4f9d9cf5974755ecad3f5bd66868
Travis Rodgers <travisdotmedia@gmail.com>
2024-08-29 16:30:33 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from rembg import remove
+import os
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.post("/removeBackground")
+async def removeBackground(file: UploadFile = File(...)):
+    # Ensure the images directory exists
+    os.makedirs('images', exist_ok=True)
+
+    name = file.filename
+    input_path = os.path.join('images', name)
+    output_path = os.path.join('images', name + '_bgremoved.png')
+
+    # Save the uploaded file content to a temporary file
+    with open(input_path, 'wb') as f:
+        f.write(await file.read())
+
+    # Process the file to remove the background
+    with open(input_path, 'rb') as i:
+        with open(output_path, 'wb') as o:
+            input = i.read()
+            output = remove(input)
+            o.write(output)
+
+    return FileResponse(output_path, media_type='image/png', filename=output_path)
