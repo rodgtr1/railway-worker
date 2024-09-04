7988947011ce170e8632c8e26d39e6ac36e4c459
Travis Rodgers <travisdotmedia@gmail.com>
2024-09-04 14:53:57 -0400

    removed duplicates

--- main.py
+++ main.py
@@ -59,35 +59,3 @@ async def removeBackground(thumbnail: UploadFile = File(...)):
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         return {"error": "An error occurred while processing the file."}
-
-@app.post("/removeBackground")
-async def removeBackground(thumbnail: UploadFile = File(...)):
-
-    logger.info(f"processing ${thumbnail.filename}")
-    try:
-        # Ensure the images directory exists
-        os.makedirs('images', exist_ok=True)
-        logger.info("images folder created")
-
-        name = thumbnail.filename
-        input_path = os.path.join('images', name)
-        output_path = os.path.join('images', name + '_bgremoved.png')
-
-        # Save the uploaded file content to a temporary file
-        with open(input_path, 'wb') as f:
-            f.write(await thumbnail.read())
-            logger.info("original image saved")
-
-        # Process the file to remove the background
-        with open(input_path, 'rb') as i:
-            with open(output_path, 'wb') as o:
-                input = i.read()
-                output = remove(input)
-                o.write(output)
-                logger.info("background removed successfully")
-
-        return FileResponse(output_path, media_type='image/png', filename=output_path)
-    
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-        return {"error": "An error occurred while processing the file."}
